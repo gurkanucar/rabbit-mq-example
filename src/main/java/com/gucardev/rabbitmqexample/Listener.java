b0fd4bfbcae348555e7c81411da621897dd63d92
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-09-08 11:24:26 +0300

    feat: random delay implemented

--- src/main/java/com/gucardev/rabbitmqexample/Listener.java
+++ src/main/java/com/gucardev/rabbitmqexample/Listener.java
@@ -1,5 +1,6 @@
 package com.gucardev.rabbitmqexample;
 
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -12,10 +13,17 @@ import org.springframework.stereotype.Service;
 public class Listener {
   private final RabbitTemplate rabbitTemplate;
 
+  private final Random random = new Random();
+
   @RabbitListener(queues = Constants.QUEUE)
-  public void consumeMessageFromQueue(MyData obj) {
+  public void consumeMessageFromQueue(MyData obj) throws InterruptedException {
+
     log.info("Message Received from queue: {} ", obj);
     obj.setName(obj.getName() + "_UPDATED");
+
+    int randomDelay = 3000 + random.nextInt(3000);
+    Thread.sleep(randomDelay);
+
     rabbitTemplate.convertAndSend(Constants.EXCHANGE, Constants.ROUTING_KEY2, obj);
   }
 }
