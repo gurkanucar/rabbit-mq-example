cb13534279fb0eb78fd6d133f5b0fa67c7e8a892
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-09-08 11:06:39 +0300

    feat: example producer and consumer created

---
+++ src/main/java/com/gucardev/rabbitmqexample/Controller.java
@@ -0,0 +1,23 @@
+package com.gucardev.rabbitmqexample;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/test")
+@RequiredArgsConstructor
+public class Controller {
+  private final RabbitTemplate rabbitTemplate;
+
+  @GetMapping("/{name}")
+  public String bookOrder(@PathVariable String name) {
+    rabbitTemplate.convertAndSend(Constants.EXCHANGE, Constants.ROUTING_KEY, new MyData(name));
+    return "success!!";
+  }
+}
